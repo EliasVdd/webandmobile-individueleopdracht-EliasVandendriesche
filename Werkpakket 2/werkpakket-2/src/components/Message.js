4e593fd10a235bdfa3247a569f4d1e8a3870bd0c
Michiel Gabriels <Michiel.Gabriels@student.pxl.be>
2018-10-12 09:58:02 +0200

    Change MessageList component

--- Werkpakket 2/werkpakket-2/src/components/Message.js
+++ Werkpakket 2/werkpakket-2/src/components/Message.js
@@ -1,41 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
 import '../styles/MessageCard.css'
-import { Button, Card, CardText, CardTitle, CardActions, Textfield, FABButton, Icon } from 'react-mdl';
 import  'react-mdl/extra/material.js'
 import  'react-mdl/extra/material.css'
 
 const Message = (props) => {
-    return (
-        <div>
-            <Card shadow={0} style={{ maxWidth: '512px', width: 'auto', margin: 'auto', marginTop: '20px', marginBottom: '20px' }}>
-                <CardTitle style={{ color: '#fff', minHeight: '50px', height: 'auto', background: 'grey' }}>
-                    Category: {props.messageModel.category}, Upvotes: {props.messageModel.upvotes}, Downvotes: {props.messageModel.downvotes}
-                </CardTitle>
-                <CardText style={{ textAlign: 'left', minHeight: '100px' }}>
-                    {props.messageModel.content}
-                </CardText>
-                <CardActions border style={{ background: 'lightgrey' }}>
-                    <div style={{ float: 'left', width: '60%' }}>
-                        <Textfield onChange={() => {}} label="Reaction content..." rows={3} style={{ width: 'parent' }}/>
-                        <p><Button raised ripple style={{ float: 'right' }}>Place reaction</Button></p>
-                    </div>
-                    <div style={{ float: 'right', width: '40%' }}>
-                        <p style={{ textAlign: 'right' }}>
-                            <FABButton raised ripple>
-                                <Icon name="exposure_plus_1"/>
-                            </FABButton>
-                        </p>
-                        <p style={{ textAlign: 'right' }}>
-                            <FABButton raised ripple>
-                                <Icon name="exposure_neg_1"/>
-                            </FABButton>
-                        </p>
-                    </div>
-                </CardActions>
-            </Card>
-        </div>
-    )
+    
 }
 
 Message.PropTypes = {
--- Werkpakket 2/werkpakket-2/src/components/MessageList.js
+++ Werkpakket 2/werkpakket-2/src/components/MessageList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button, Card, CardText, CardTitle, CardActions, Textfield, FABButton, Icon } from 'react-mdl';
 import PropTypes from "prop-types";
 import './Message';
 import Message from './Message';
@@ -25,8 +26,34 @@ class MessageList extends Component {
     renderMessages() {
         return this.state.messages.map(message => 
             (
-                <Message
-                messageModel={message}/>
+                <div>
+                    <Card shadow={0} style={{ maxWidth: '512px', width: 'auto', margin: 'auto', marginTop: '20px', marginBottom: '20px' }}>
+                        <CardTitle style={{ color: '#fff', minHeight: '50px', height: 'auto', background: 'grey' }}>
+                            Category: {message.category}, Upvotes: {message.upvotes}, Downvotes: {message.downvotes}
+                        </CardTitle>
+                        <CardText style={{ textAlign: 'left', minHeight: '100px' }}>
+                            {message.content}
+                        </CardText>
+                        <CardActions border style={{ background: 'lightgrey' }}>
+                            <div style={{ float: 'left', width: '60%' }}>
+                                <Textfield onChange={() => { }} label="Reaction content..." rows={3} style={{ width: 'parent' }} />
+                                <p><Button raised ripple style={{ float: 'right' }}>Place reaction</Button></p>
+                            </div>
+                            <div style={{ float: 'right', width: '40%' }}>
+                                <p style={{ textAlign: 'right' }}>
+                                    <FABButton raised ripple onClickUpvote={this.onClickUpvote}>
+                                        <Icon name="exposure_plus_1" />
+                                    </FABButton>
+                                </p>
+                                <p style={{ textAlign: 'right' }}>
+                                    <FABButton raised ripple>
+                                        <Icon name="exposure_neg_1" />
+                                    </FABButton>
+                                </p>
+                            </div>
+                        </CardActions>
+                    </Card>
+                </div>
             ),
         )
     }
