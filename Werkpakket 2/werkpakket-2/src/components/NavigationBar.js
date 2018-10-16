a14ea25dcc4211afb65d203cd2f3336a1cb25e66
Michiel Gabriels <Michiel.Gabriels@student.pxl.be>
2018-10-16 11:19:14 +0200

    Auto stash before merge of "master" and "origin/master"

--- Werkpakket 2/werkpakket-2/src/components/NavigationBar.js
+++ Werkpakket 2/werkpakket-2/src/components/NavigationBar.js
@@ -1,42 +1,32 @@
-import React, { Component } from 'react';
-import PropTypes from "prop-types";
+import React from 'react';
 import { Textfield, Layout, Header, HeaderRow, Navigation, Drawer, Content } from 'react-mdl';
 
 const NavigationBar = (props) => {
     return (
         <div>
-            <div className="demo-big-content">
-    <Layout>
-        <Header waterfall>
-            <HeaderRow title="Title">
-                <Textfield
-                    value="Search"
-                    onChange={() => {}}
-                    label="Search2"
-                    expandable
-                    expandableIcon="search"
-                />
-            </HeaderRow>
-            <HeaderRow>
-                <Navigation>
-                    <a href="#">Find by id</a>
-                </Navigation>
-            </HeaderRow>
-        </Header>
-        <Drawer title="Title">
-            <Navigation>
-                <a href="#">Find by id</a>
-            </Navigation>
-        </Drawer>
-        <Content>
-            <div className="page-content" />
-        </Content>
-    </Layout>
-</div>
-</div>
+            <Layout>
+                <Header>
+                    <HeaderRow title="Title">
+                        <Textfield
+                            value="Search"
+                            onChange={() => { }}
+                            label="Search2"
+                            expandable
+                            expandableIcon="search"
+                        />
+                    </HeaderRow>
+                </Header>
+                <Drawer title="Title">
+                    <Navigation>
+                        <a href="#">Find by id</a>
+                    </Navigation>
+                </Drawer>
+                <Content>
+                    <div className="page-content" />
+                </Content>
+            </Layout>
+        </div>
     );
 }
-
-
 
 export default NavigationBar;
