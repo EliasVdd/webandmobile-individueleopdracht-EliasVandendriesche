f11616c23c9e865bec1db4a75da140ab7c8f26bc
JonasKempeneers <jonaskempeneers193@gmail.com>
2018-10-18 11:04:18 +0200

    Added redux files and added dashboard component
    I added the redux files (store, ...). I also added some material ui components.

--- Werkpakket 2/werkpakket-2/src/App.js
+++ Werkpakket 2/werkpakket-2/src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import MessageList from './components/MessageList.js';
-import NavigationBar from './components/NavigationBar.js';
+import Dashboard from './components/Dashboard';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavigationBar />
-        <MessageList />
-      </div>
+      <Dashboard/>
     );
   }
 }
---
+++ Werkpakket 2/werkpakket-2/src/components/Dashboard.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import MessageList from './MessageList.js';
+import NavigationBar from './NavigationBar.js';
+import Welcome from './Welcome.js';
+import { Switch, Route } from 'react-router-dom';
+import axios from 'axios';
+
+class Dashboard extends Component {
+    state = {
+        
+    }
+
+    render() {
+        return (
+    <div className="App">
+        <NavigationBar
+        onSearchContentSubmit={this.onSearchContentSubmit}
+        />
+
+        <Switch>
+          <Route exact path='/' component={Welcome} />
+          <Route path='/messageList' component={MessageList} />
+        </Switch>
+    </div>
+        );
+    }
+
+    getMessagesWithContent(){
+        var content = 'dolor';
+        axios.get('http://localhost:8000/messages/find/' + content)
+        .then(response => {
+            const messages = response.data;
+            this.setState({ messages })
+        })
+
+    }
+}
+
+export default Dashboard;
