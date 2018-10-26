0e12a73571dc2f6648b52e72b21d2b38d4a69491
JonasKempeneers <jonaskempeneers193@gmail.com>
2018-10-26 11:07:13 +0200

    Added Message functionality
    I added functionality to add and view messages

---
+++ Werkpakket 3/werkpakket3/src/Controller/MessageController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\MessageType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Message;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\User;
+
+class MessageController extends AbstractController
+{
+    public function __construct()
+    {
+    }
+
+    /**
+     * @Route("/messages", name="messages")
+     */
+    public function index(Request $request)
+    {
+        $messages = $this->getDoctrine()
+            ->getRepository(Message::class)
+            ->findAll();
+
+        return $this->render('message/index.html.twig', array('messages'=>$messages));
+    }
+
+    /**
+     * @Route("/message", methods={"GET", "POST"} , name="getmessage")
+     */
+    public function showMessageForm(Request $request)
+    {
+        $form = $this->createForm(MessageType::class, new Message());
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $message = new Message();
+            $message->setContent($form->getData());
+            $message->setUserid($this->getUser()->getId());
+
+            $this->postMessage($message);
+
+            return $this->redirectToRoute('messages');
+        }
+
+        return $this->render(
+            'message/addMessage.html.twig',
+            array('form' => $form->createView())
+        );
+    }
+
+    public function postMessage(Message $message)
+    {
+        $emManager = $this->getDoctrine()->getManager();
+
+        $emManager->persist($message);
+        $emManager->flush();
+    }
+
+}
