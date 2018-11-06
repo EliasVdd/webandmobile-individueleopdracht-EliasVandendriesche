4eb341dce44e125f4fccfd5a52db64c1d827b007
JonasKempeneers <jonaskempeneers193@gmail.com>
2018-11-06 11:41:29 +0100

    Added functional tests for MessageController and CategoryController

--- Werkpakket 3/werkpakket3/tests/Controller/CategoryControllerTest.php
+++ Werkpakket 3/werkpakket3/tests/Controller/CategoryControllerTest.php
@@ -9,21 +9,66 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Tests\Helpers\LoginHelper;
 
 class CategoryControllerTest extends WebTestCase
 {
     private $client = null;
+    private $loginHelper = null;
 
     public function setUp()
     {
         $this->client = static::createClient();
+        $this->loginHelper = new LoginHelper();
     }
 
-    public function testGetCategoriesIndex()
+    //Jonas
+    public function testGetCategoriesIndexReturnsRedirectWhenAnonymous()
     {
         $this->client->request('GET', '/categories');
 
         $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+    }
+
+    //Jonas
+    public function testGetCategoriesIndexReturns200WhenAdminOrModerator()
+    {
+        $this->loginHelper->loginAdmin($this->client);
+
+        $this->client->request('GET', '/categories');
+
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    //Jonas
+    public function testGetCategoryFormReturnsFormWhenModerator()
+    {
+        $this->loginHelper->loginAdmin($this->client);
+
+        $crawler = $this->client->request('GET', '/category');
+
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('h1.display-4:contains("Create a new category")')->count()
+        );
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('label:contains("Name")')->count()
+        );
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('button:contains("Save")')->count()
+        );
+    }
+
+    //Jonas
+    public function testGetCategoryFormReturns200WhenModerator()
+    {
+        $this->loginHelper->loginModerator($this->client);
+
+        $this->client->request('GET', '/category');
+
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
     }
 
 }
--- Werkpakket 3/werkpakket3/tests/Controller/MessageControllerTest.php
+++ Werkpakket 3/werkpakket3/tests/Controller/MessageControllerTest.php
@@ -9,6 +9,8 @@
 namespace App\Tests\Controller;
 
 
+use App\Controller\MessageController;
+use App\Entity\Message;
 use App\Tests\Helpers\LoginHelper;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
@@ -23,6 +25,7 @@ class MessageControllerTest extends WebTestCase
         $this->loginHelper = new LoginHelper();
     }
 
+    //Jonas
     public function testGetMessageListWhileAnonymous()
     {
         $this->client->request('GET', '/messages');
@@ -30,6 +33,7 @@ class MessageControllerTest extends WebTestCase
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
     }
 
+    //Jonas
     public function testGetMessageList()
     {
         $this->loginHelper->logInUser($this->client);
@@ -38,14 +42,54 @@ class MessageControllerTest extends WebTestCase
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
     }
 
+    //Jonas
     public function testShowMessageIndexTemplate()
     {
         $this->loginHelper->logInUser($this->client);
 
         $crawler = $this->client->request('GET', '/messages');
 
-        $this->assertTrue(true);
-
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('a:contains("Add Message")')->count()
+        );
     }
+
+    //Jonas
+    public function testShowAddMessageForm()
+    {
+        $this->loginHelper->loginUser($this->client);
+
+        $crawler = $this->client->request('GET', '/message');
+
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('h1:contains("Create new message")')->count()
+        );
+    }
+
+    //Jonas
+    public function testShowAddMessageFormReturnAccessDeniedPageWhenNotAuthenticated()
+    {
+        $crawler = $this->client->request('GET', '/message');
+
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('h1:contains("Access denied")')->count()
+        );
+    }
+
+    //Jonas
+    public function testAdminGetsOptionToDeleteAndUpdateMessages()
+    {
+        $this->loginHelper->loginAdmin($this->client);
+
+        $crawler = $this->client->request('GET', '/messages');
+
+        $this->assertGreaterThan(0, $crawler->filter('a.fa-trash-alt')->count());
+        $this->assertGreaterThan(0, $crawler->filter('a.fa-pencil-alt')->count());
+    }
+
+
 
 }
